d6a7c8fa1230c419acd876e63450ca23c4729e8b
Isabella <wileyisabella@gmail.com>
2021-04-28 11:13:27 -0400

    finished!

--- lib/person.rb
+++ lib/person.rb
@@ -54,6 +54,32 @@ class Person
         '♪ Rub-a-dub just relaxing in the tub ♫'
     end
 
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return 'blah blah partisan blah lobbyist'
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return 'blah blah sun blah rain'
+        else
+            return 'blah blah blah blah blah'
+        end
+    end
+
     
 end
 
